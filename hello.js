586a057e68ba35193440c9f8eceea93e4947b0bc
Daniel Kang <me@daniel.gs>
2016-11-01 10:40:57 -0700

    add env vars

--- hello.js
+++ hello.js
@@ -4,9 +4,12 @@ const hostname = '0.0.0.0';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
+    let message = process.env.MESSAGE || 'Hello, World';
+    let contentType = process.env.CONTENT_TYPE || 'text/plain';
+
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
+    res.setHeader('Content-Type', contentType);
+    res.end(message + '\n');
 });
 
 server.listen(port, hostname, () => {
